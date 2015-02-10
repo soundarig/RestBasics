c051f9e52fdab70ed47c9343236f350d04ca0b80
nssanthoshkumar <santhosh.kumar2@emc.com>
2015-02-10 11:23:12 +0530

    corrected the create table script

--- src/main/resources/schema-cassandra.sql
+++ src/main/resources/schema-cassandra.sql
@@ -1,16 +1,9 @@
 --CREATE KEYSPACE vsfnkeyspace WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
  
  
-drop table  if exists  employee;
+drop table if exists employee;
 
-CREATE TABLE employee (                                 
-           employeenumber BIGINT,   
-           name varchar(50) DEFAULT NULL,                    
-           city varchar(50) DEFAULT NULL,                       
-           country varchar(50) DEFAULT NULL,                   
-           emailaddress varchar(50) DEFAULT NULL,               
-           CONSTRAINT PK_employee PRIMARY KEY (employeeNumber)                         
-         );
+CREATE TABLE employee(employeeNumber bigint, name text, city text, country text, emailaddress text, primary key((employeeNumber)));
 
 insert into employee(employeenumber, name,city,country,emailaddress) values(112233, 'soundari','Bangalore','India','gnanasoundari.soundarajan@emc.com');
 
